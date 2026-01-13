e5e6b8df894d25e934afb4f5b018076f764a0931
a6hijeet <abhijeetsatpute.dev@gmail.com>
2026-01-13 16:37:24 +0530

    Jackson: CRUD operation / stored in json file

--- repository/impl/PostRepository.java
+++ repository/impl/PostRepository.java
@@ -1,9 +1,5 @@
 package repository.impl;
 
-import java.nio.file.Path;
-import java.nio.file.Paths;
-import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.List;
 
 import com.fasterxml.jackson.core.type.TypeReference;
@@ -20,18 +16,6 @@ public class PostRepository {
         return posts;
     }
 
-     /* public boolean addPost(Post p) {
-        JsonFile.writeList(FILE, Arrays.asList(p));
-        return this.postList.add(p);
-    }
-
-    public void updatePost(int index, Post p) {
-        this.postList.set(index, p);
-    }
-
-    public Post deletePost(int index) {
-        return this.postList.remove(index);
-    } */
    public void saveAll(List<Post> posts) {
     JsonFile.writeList(FILE, posts);
    }
